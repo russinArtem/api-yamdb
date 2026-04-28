6d5deb8059cdc8a162fd76ddf358c5ae7fbbdd4d
Sgt.PepeF_main <Sgt.PepeF@yandex.ru>
2026-04-28 16:39:32 +0500

    Переписаны модели, добавлен скрипт на инсерт данных из csv

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -6,7 +6,7 @@ from django.db import models
 User = get_user_model()
 
 
-class Categories(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=256)
     slug = models.SlugField(max_length=50, unique=True)
 
@@ -14,7 +14,7 @@ class Categories(models.Model):
         return self.name
 
 
-class Genres(models.Model):
+class Genre(models.Model):
     name = models.CharField(max_length=256)
     slug = models.SlugField(max_length=50, unique=True)
 
@@ -22,30 +22,31 @@ class Genres(models.Model):
         return self.name
 
 
-class Titles(models.Model):
-    category = models.ForeignKey(Categories, related_name='titles', on_delete=models.SET_NULL, blank=True, null=True)
-    genre = models.ManyToManyField(Genres, through='GenreTitle', related_name='titles')
+class Title(models.Model):
+    category = models.ForeignKey(Category, related_name='titles', on_delete=models.SET_NULL, blank=True, null=True)
+    genre = models.ManyToManyField(Genre, through='GenreTitle', related_name='titles', blank=True, null=True)
     name = models.CharField(max_length=256)
-    year = models.IntegerField()
-    description = models.TextField()
+    year = models.IntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 
 class GenreTitle(models.Model):
-    genre = models.ForeignKey(Genres, on_delete=models.CASCADE)
-    title = models.ForeignKey(Titles, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
 
 
 class ContentModel(models.Model):
     """Родительский класс моделей контента."""
 
-    author = models.ForeignKey(User, related_name='rewiews', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     pub_date = models.DateTimeField(verbose_name='Дата публикации', auto_now_add=True)
     text = models.TextField()
 
     class Meta:
+        abstract = True
         ordering = (
             '-pub_date',
             'author',
@@ -55,8 +56,8 @@ class ContentModel(models.Model):
         return self.text
 
 
-class Reviews(ContentModel):
-    title = models.ForeignKey(Titles, related_name='rewiews', on_delete=models.CASCADE)
+class Review(ContentModel):
+    title = models.ForeignKey(Title, related_name='rewiews', on_delete=models.CASCADE)
     score = models.IntegerField(
         validators=(
             MinValueValidator(1),
@@ -65,5 +66,5 @@ class Reviews(ContentModel):
     )
 
 
-class Comments(ContentModel):
-    review = models.ForeignKey(Reviews, related_name='comments', on_delete=models.CASCADE)
+class Comment(ContentModel):
+    review = models.ForeignKey(Review, related_name='comments', on_delete=models.CASCADE)

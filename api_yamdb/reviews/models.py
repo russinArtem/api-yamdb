cd7c1907273333394155c768a0cac228904550e6
Artem Russin <russinartem@yandex.ru>
2026-04-30 23:26:55 +0300

    Выполнены задачи второго и третьего разработчиков. Все 33 теста проходят.

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
     name = models.CharField(max_length=256)
     slug = models.SlugField(max_length=50, unique=True)
 
+    class Meta:
+        ordering = ('name',)
+
     def __str__(self):
         return self.name
 
@@ -18,31 +21,68 @@ class Genre(models.Model):
     name = models.CharField(max_length=256)
     slug = models.SlugField(max_length=50, unique=True)
 
+    class Meta:
+        ordering = ('name',)
+
     def __str__(self):
         return self.name
 
 
 class Title(models.Model):
-    category = models.ForeignKey(Category, related_name='titles', on_delete=models.SET_NULL, blank=True, null=True)
-    genre = models.ManyToManyField(Genre, through='GenreTitle', related_name='titles', blank=True, null=True)
+    category = models.ForeignKey(
+        Category, related_name='titles',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        through='GenreTitle',
+        related_name='titles',
+        blank=True
+    )
     name = models.CharField(max_length=256)
     year = models.IntegerField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
+    rating = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name='Рейтинг'
+    )
+
+    class Meta:
+        ordering = ('-year',)
 
     def __str__(self):
         return self.name
+
+    def update_rating(self):
+        avg_rating = self.reviews.aggregate(models.Avg('score'))['score__avg']
+        self.rating = round(avg_rating, 1) if avg_rating else None
+        self.save(update_fields=['rating'])
 
 
 class GenreTitle(models.Model):
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     title = models.ForeignKey(Title, on_delete=models.CASCADE)
 
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=('genre', 'title'),
+                name='Unique constraint',
+            ),
+        ]
+
 
 class ContentModel(models.Model):
     """Родительский класс моделей контента."""
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    pub_date = models.DateTimeField(verbose_name='Дата публикации', auto_now_add=True)
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True
+    )
     text = models.TextField()
 
     class Meta:
@@ -57,7 +97,11 @@ class ContentModel(models.Model):
 
 
 class Review(ContentModel):
-    title = models.ForeignKey(Title, related_name='rewiews', on_delete=models.CASCADE)
+    title = models.ForeignKey(
+        Title,
+        related_name='reviews',
+        on_delete=models.CASCADE
+    )
     score = models.IntegerField(
         validators=(
             MinValueValidator(1),
@@ -65,6 +109,22 @@ class Review(ContentModel):
         )
     )
 
+    class Meta:
+        ordering = (
+            '-pub_date',
+            'author',
+        )
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique_review_per_author'
+            )
+        ]
+
 
 class Comment(ContentModel):
-    review = models.ForeignKey(Review, related_name='comments', on_delete=models.CASCADE)
+    review = models.ForeignKey(
+        Review,
+        related_name='comments',
+        on_delete=models.CASCADE
+    )
